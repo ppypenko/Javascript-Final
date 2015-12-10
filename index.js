4128860927d8bebee1c729004358258208fdb4e5
Brandon Berning <berningbrandon9@gmail.com>
2015-12-10 10:42:59 -0700

    added changed to tim/date in the header. Also added drop down selection

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var dd = today.getDate();
 var mm = today.getMonth() + 1;
 var yyyy = today.getFullYear();
 var request = new XMLHttpRequest();
+var weatherData;
+
 
 //weatherApp h1
 function createElements() {
-
 
     var city = document.createElement("H1")
     var c = document.createTextNode("");
@@ -28,30 +29,40 @@ function createElements() {
 }
 
 
-function getCurrentDate() {
-    if (dd < 10) {
-        dd = '0' + dd
-    }
+//function getCurrentDate() {
+//    if (dd < 10) {
+//        dd = '0' + dd
+//    }
+//
+//    if (mm < 10) {
+//        mm = '0' + mm
+//    }
+//
+//    today = mm + '/' + dd + '/' + yyyy;
+//    document.getElementById("headerDate").innerHTML = today;
+//}
+//
+function getCurrentTime() {
+    var d = new Date();
+    d.getHours();
+    d.getMinutes();
+    d.getSeconds();
 
-    if (mm < 10) {
-        mm = '0' + mm
-    }
-
-    today = mm + '/' + dd + '/' + yyyy;
-    document.getElementById("headerDate").innerHTML = today;
+    document.getElementById("headerTime").innerHTML = d;
 }
-var weatherData;
-var request = new XMLHttpRequest();
+
 google.load("feeds", "1");
-function OnLoad(){
+
+function OnLoad() {
     var query = 'site: cnn.com president';
     google.feeds.findFeeds(query, findDone);
 }
-function findDone(result){
-    if(!result.error){
+
+function findDone(result) {
+    if (!result.error) {
         var container = document.getElementById("rssApp");
         var html = '';
-        for(var i = 0; i < result.entries.length; i++){
+        for (var i = 0; i < result.entries.length; i++) {
             var entry = result.entries[i];
             html += '<p><a href="/feed/v1/' + entry.url + '">' + entry.title + '</a></p>';
         }
@@ -60,42 +71,12 @@ function findDone(result){
 }
 google.setOnLoadCallback(OnLoad);
 
-
-//var feedcontainer = document.getElementById("rssApp"),
-//    feedurl = "http://fastpshb.appspot.com/feed/1/fastpshb",
-//    feedlimit = 10,
-//    rssoutput = "<h2>BBC News:</h2><ul>";
-//
-//function displayfeed(){
-//    var feedpointer = new google.feeds.Feed(feedurl);
-//    feedpointer.setNumEntries(feedlimit);
-//    feedpointer.load(displayfeed);
-//};
-//function displayfeed(result){
-//    if(!result.error){
-//        var thefeeds = result.feed.entries;
-//        for(var i = 0; i < thefeeds.length; i += 1){
-//            rssoutput += "<li><a href='"+thefeeds[i].link+"'>"+thefeeds[i].title+"</a></li>";   
-//        }
-//        rssoutput += "</ul>";
-//        feedcontainer.innerHTML = rssoutput;
-//    }else{
-//        alert("Error fetching feeds!");
-//    }
-//};
-//window.onload = function(){
-//    rssfeedsetup();    
-//};
-
-
-
-
 function loadData() {
 
     var cityinput = 'salt lake city';
     var regioninput = 'ut';
 
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + ',' + regioninput + '&APPID=ae41cf3cccc31ba9eb00adba1ac0e932');
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + ',' + regioninput + '&&APPID=ae41cf3cccc31ba9eb00adba1ac0e932&units=imperial');
     request.onload = loadComplete;
     request.send();
 }
@@ -109,13 +90,13 @@ function loadComplete(evt) {
 
     console.log(weatherData);
     kelvin = weatherData.main.temp;
-    fahrenheit = kelvin * (9 / 5) - 459.67;
 
     document.getElementById("city").innerHTML = "City: " + weatherData.name;
     document.getElementById("weather").innerHTML = "Weather Description: " + weatherData.weather[0].description;
-    document.getElementById("temp").innerHTML = "Temperature: " + fahrenheit.toFixed(2) + "&deg; F";
+    document.getElementById("temp").innerHTML = "Temperature: " + kelvin.toFixed(2) + "&deg; F";
 }
 
 createElements();
-getCurrentDate();
+//getCurrentDate();
+getCurrentTime();
 loadData();
