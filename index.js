ffce18289dd13441e7999d4d47a4c5ce74a74810
pyotr pypenko <pyotrspypenko@gmail.com>
2015-12-09 17:06:51 -0700

    Started on rss.

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+var weatherData;
+var request = new XMLHttpRequest();
+google.load("feeds", "1");
+function OnLoad(){
+    var query = 'site: cnn.com president';
+    google.feeds.findFeeds(query, findDone);
+}
+function findDone(result){
+    if(!result.error){
+        var container = document.getElementById("rssApp");
+        var html = '';
+        for(var i = 0; i < result.entries.length; i++){
+            var entry = result.entries[i];
+            html += '<p><a href="/feed/v1/' + entry.url + '">' + entry.title + '</a></p>';
+        }
+        container.innerHTML = html;
+    }
+}
+google.setOnLoadCallback(OnLoad);
+
+
+//var feedcontainer = document.getElementById("rssApp"),
+//    feedurl = "http://fastpshb.appspot.com/feed/1/fastpshb",
+//    feedlimit = 10,
+//    rssoutput = "<h2>BBC News:</h2><ul>";
+//
+//function displayfeed(){
+//    var feedpointer = new google.feeds.Feed(feedurl);
+//    feedpointer.setNumEntries(feedlimit);
+//    feedpointer.load(displayfeed);
+//};
+//function displayfeed(result){
+//    if(!result.error){
+//        var thefeeds = result.feed.entries;
+//        for(var i = 0; i < thefeeds.length; i += 1){
+//            rssoutput += "<li><a href='"+thefeeds[i].link+"'>"+thefeeds[i].title+"</a></li>";   
+//        }
+//        rssoutput += "</ul>";
+//        feedcontainer.innerHTML = rssoutput;
+//    }else{
+//        alert("Error fetching feeds!");
+//    }
+//};
+//window.onload = function(){
+//    rssfeedsetup();    
+//};
+
+
+
+
 function loadData() {
     
     var cityinput = document.getElementById("citybox").value;
@@ -22,3 +72,4 @@ function loadComplete(evt) {
     document.getElementById("weather0").innerHTML = "Weather Description: "+weatherData.weather[0].description;
     document.getElementById("temp0").innerHTML = "Temperature: "+ fahrenheit.toFixed(2)+"&deg; F";
 }
+
