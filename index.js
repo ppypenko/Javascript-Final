b816a175742368ac4ee0331d826fd87265742ef1
Brandon Berning <berningbrandon9@gmail.com>
2015-12-11 10:03:14 -0700

    changed some stuff

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var weatherData,
     dd = today.getDate(),
     mm = today.getMonth() + 1,
     yyyy = today.getFullYear(),
+    cityinput = 'salt lake city',
+    regioninput = 'ut',
     weatherData;
 
 
@@ -30,22 +32,14 @@ function createElements() {
     document.getElementById("weatherApp").appendChild(temp);
     temp.appendChild(t);
     temp.setAttribute('id', 'temp');
+
+    var wind = document.createElement("H1"),
+        w = document.createTextNode("");
+    document.getElementById("weatherApp").appendChild(wind);
+    wind.appendChild(w);
+    wind.setAttribute('id', 'wind');
 }
 
-
-//function getCurrentDate() {
-//    if (dd < 10) {
-//        dd = '0' + dd
-//    }
-//
-//    if (mm < 10) {
-//        mm = '0' + mm
-//    }
-//
-//    today = mm + '/' + dd + '/' + yyyy;
-//    document.getElementById("headerDate").innerHTML = today;
-//}
-//
 function getCurrentTime() {
     var d = new Date();
     d.getHours();
@@ -56,11 +50,17 @@ function getCurrentTime() {
 }
 
 
+function changePlace() {
+    cityinput = document.getElementById("txtCity").value;
+    regioninput = document.getElementById("txtRegion").value;
+    loadData();
+
+}
 
 $.ajax({
-    url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("http://feeds.bbci.co.uk/news/rss.xml?edition=uk"),
-    dataType : 'json',
-    success  : function (data) {
+    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("http://feeds.bbci.co.uk/news/rss.xml?edition=uk"),
+    dataType: 'json',
+    success: function (data) {
         if (data.responseData.feed && data.responseData.feed.entries) {
             var rsspage = "<h2>BBC News</h2>";
             $.each(data.responseData.feed.entries, function (i, e) {
@@ -75,8 +75,7 @@ $.ajax({
 
 function loadData() {
 
-    var cityinput = 'salt lake city',
-        regioninput = 'ut';
+    //    regioninput = 'ut';
 
     request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + ',' + regioninput + '&&APPID=ae41cf3cccc31ba9eb00adba1ac0e932&units=imperial');
     request.onload = loadComplete;
@@ -85,17 +84,19 @@ function loadData() {
 
 
 function loadComplete(evt) {
-    var fahrenheit = 0,
-        kelvin = 0;
-
     weatherData = JSON.parse(request.responseText);
 
     console.log(weatherData);
-    kelvin = weatherData.main.temp;
+    var kelvin = weatherData.main.temp;
 
+    console.log(weatherData);
     document.getElementById("city").innerHTML = "City: " + weatherData.name;
     document.getElementById("weather").innerHTML = "Weather Description: " + weatherData.weather[0].description;
     document.getElementById("temp").innerHTML = "Temperature: " + kelvin.toFixed(2) + "&deg; F";
+    document.getElementById("wind").innerHTML = "Wind Speeds: " + weatherData.wind.speed + " mph";
+
+
+
 }
 
 createElements();
